76c7f0c5e21a3c72d572360ae9d51ace1e39b1c1
fabioaraujow <araujobimmer@gmail.com>
2026-09-19 12:29:01 +0100

    under construction

--- ex3/ft_vault_security.py
+++ ex3/ft_vault_security.py
@@ -1,1 +1,12 @@
 #!/usr/bin/env python3
+
+def secure_archive(data: str) -> tuple[bool, str]:
+    boolean = False
+    result = ""
+    try:
+        with open(data) as o_data:
+            result = o_data.read()
+        boolean = True
+    except OSError as err:
+        result = str(err)
+    return boolean, result
